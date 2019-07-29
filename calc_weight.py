123f6fcf9a21b92869f0f82af0335aef702cf5a3
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-07-30 05:16:20 +0900

    Update calc_weight.py

--- calc_weight.py
+++ calc_weight.py
@@ -1,11 +1,18 @@
 import torch
 
-def calc_weight(Tb, n_class=2):
+def calc_weight(Tb, n_class):
   weights = torch.zeros_like(Tb).to(torch.float)
+  recip_probs = torch.zeros_like(Tb).to(torch.float)
   counts = torch.zeros(n_class)
+
   for i in range(n_class):
     counts[i] += (Tb == i).sum()
   probs = counts / counts.sum()
-  weights = probs ** (-1)
-  weights /= weights.sum()
+  recip_probs = probs ** (-1)
+
+  for i in range(n_class):
+    mask = (Tb == i)
+    weights[mask] = recip_probs[i]
+
+  weights /= weights.sum() # normalize for stabilizing loss across batches
   return weights
