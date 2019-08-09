d6bb0adf0329e0b3d76a986a4ea72b6d49a75d44
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-08-09 13:01:54 +0900

    Update multi_task_loss.py

--- multi_task_loss.py
+++ multi_task_loss.py
@@ -2,18 +2,27 @@ import torch
 
 class MultiTaskLoss(torch.nn.Module):
 
-  def __init__(self, is_regression):
+  def __init__(self, is_regression, reduction='none'):
     super(MultiTaskLoss, self).__init__()
+    self.is_regression = is_regression
     self.n_tasks = len(is_regression)
-    self.log_vars = torch.nn.Parameter(torch.zeros(n_tasks))
+    self.log_vars = torch.nn.Parameter(torch.zeros(self.n_tasks))
+    self.reduction = reduction
 
   def forward(self, losses):
-    multi_task_losses = []
-    stds = torch.exp(self.log_vars)**(1/2)
-    coeffs = 1 / ( (is_regression+1)*(stds**2) )
-
+    dtype = losses.dtype
+    device = losses.device
+    stds = (torch.exp(self.log_vars)**(1/2)).to(device).to(dtype)
+    self.is_regression = self.is_regression.to(device).to(dtype)
+    coeffs = 1 / ( (self.is_regression+1)*(stds**2) )
     multi_task_losses = coeffs*losses + torch.log(stds)
-    return muti_task_losses
+
+    if self.reduction == 'sum':
+      multi_task_losses = multi_task_losses.sum()
+    if self.reduction == 'mean':
+      multi_task_losses = multi_task_losses.mean()
+
+    return multi_task_losses
 
 '''
 usage
