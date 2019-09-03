dba5ac4eeec941ec976b03b7a6526a1ccbb18ae8
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-09-03 14:46:13 +0900

    Update pdf_loss.py

--- pdf_loss.py
+++ pdf_loss.py
@@ -5,5 +5,5 @@ def pdf(x, mu, sigma): # probability density function
   var = sigma**2
   return 1./((2*math.pi*var)**0.5) * torch.exp(-(x-mu)**2 / (2*var))
 
-def pdf_loss(x, mu, sigma, epsilon=1e-5):
+def PDFLoss(x, mu, sigma, epsilon=1e-5):
   return - torch.log(pdf(x, mu, sigma) + epsilon)
