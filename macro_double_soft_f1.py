debd4802af5e206b18101a0ec9e33a70f8b7ce18
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2020-01-09 15:02:35 +0900

    Update macro_double_soft_f1.py

--- macro_double_soft_f1.py
+++ macro_double_soft_f1.py
@@ -1,12 +1,17 @@
-def macro_double_soft_f1_tf(y, y_hat): # https://towardsdatascience.com/the-unknown-benefits-of-using-a-soft-f1-loss-in-classification-systems-753902c0105d
+import tensorflow as tf
+import torch
+
+
+def macro_double_soft_f1_tf(y, y_hat):
+    # https://towardsdatascience.com/the-unknown-benefits-of-using-a-soft-f1-loss-in-classification-systems-753902c0105d
     """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
     Use probability values instead of binary predictions.
     This version uses the computation of soft-F1 for both positive and negative class for each label.
-    
+
     Args:
         y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
         y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
-        
+
     Returns:
         cost (scalar Tensor): value of the cost function for the batch
     """
@@ -23,3 +28,49 @@ def macro_double_soft_f1_tf(y, y_hat): # https://towardsdatascience.com/the-unkn
     cost = 0.5 * (cost_class1 + cost_class0) # take into account both class 1 and class 0
     macro_cost = tf.reduce_mean(cost) # average on all labels
     return macro_cost
+
+
+def macro_double_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    This version uses the computation of soft-F1 for both positive and negative class for each label.
+
+    Args:
+        y (int): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+
+    Returns:
+        cost (scalar): value of the cost function for the batch
+    """
+    y = torch.FloatTensor(y)
+    y_hat = torch.FloatTensor(y_hat)
+
+    tp = (y_hat * y).sum(dim=0)
+    fp = (y_hat * (1-y)).sum(dim=0)
+    fn = ((1-y_hat) * y).sum(dim=0)
+    tn = ((1-y_hat) * (1-y)).sum(dim=0)
+
+    soft_f1_class1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    soft_f1_class0 = 2*tn / (2*tn + fn + fp + 1e-16)
+    cost_class1 = 1 - soft_f1_class1 # reduce 1 - soft-f1_class1 in order to increase soft-f1 on class 1
+    cost_class0 = 1 - soft_f1_class0 # reduce 1 - soft-f1_class0 in order to increase soft-f1 on class 0
+    cost = 0.5 * (cost_class1 + cost_class0) # take into account both class 1 and class 0
+
+    macro_cost = cost.mean() # average on all labels
+    return macro_cost
+
+
+if __name__ == '__main__':
+    BS = 64
+    y = np.random.randint(2, size=BS)
+    y_hat = np.random.rand(BS)
+
+    ## Written in Tensorflow
+    loss_tf = macro_double_soft_f1_tf(y, y_hat)
+
+    ## Written in PyTorch
+    loss_torch = macro_double_soft_f1(y, y_hat)
+
+    print('Macro Double Soft F1 Loss')
+    print('Written in Tensorflow :{}'.format(loss_tf))
+    print('Written in Pytorch :{}'.format(loss_torch))
